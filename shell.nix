3c280afa96212b9a290f7debfc7044b46540969c
Nicolas Mattia <nicolas@nmattia.com>
2018-03-29 16:07:46 +0200

    Initial commit

---
+++ shell.nix
@@ -0,0 +1,21 @@
+let
+  # Create a package set with some overlays
+  pkgs = import ./nix;
+  ihaskell = pkgs.ihaskellWithPackages
+    (ps:
+      [
+        ps.aeson
+        ps.compact
+        ps.conduit
+        ps.conduit-combinators
+        ps.ihaskell-charts
+        ps.leveldb-haskell
+        ps.plots
+        ps.wreq
+      ]
+    );
+in pkgs.mkShell
+  {
+    name = "my-jupyter";
+    buildInputs = [ ihaskell ];
+  }
